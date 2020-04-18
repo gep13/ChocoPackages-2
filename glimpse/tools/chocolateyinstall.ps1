b21d27f932ce4d2a9fb1267382cf2488f118a5ff
corbob <corbob@knoxy.ca>
2020-04-17 23:03:55 -0700

    Slight adjustments

--- glimpse/tools/chocolateyinstall.ps1
+++ glimpse/tools/chocolateyinstall.ps1
@@ -1,21 +1,21 @@
 
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/glimpse-editor/Glimpse/releases/download/v0.1.2/glimpse-0.1.2.msi'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "https://github.com/glimpse-editor/Glimpse/releases/download/v$($env:chocolateyPackageVersion)/glimpse-$($env:chocolateyPackageVersion).msi"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'msi'
-  url           = $url
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = $url
 
-  softwareName  = 'glimpse*'
+  softwareName   = 'glimpse*'
 
-  checksum      = '99C3DF2884310CB74C39A15FB63E93FBF2112F581DC5325B37123467618D89E8'
-  checksumType  = 'sha256'
+  checksum       = '99C3DF2884310CB74C39A15FB63E93FBF2112F581DC5325B37123467618D89E8'
+  checksumType   = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
