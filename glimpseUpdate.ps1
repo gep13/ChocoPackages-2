8d2f43635367a50727063cdf619a7425f8d7200b
corbob <corbob@knoxy.ca>
2020-08-25 21:58:44 -0700

    Create glimpse update script

---
+++ glimpseUpdate.ps1
@@ -0,0 +1,48 @@
+$Latest = Invoke-RestMethod 'https://api.github.com/repos/glimpse-editor/Glimpse/releases/latest'
+$Current = choco list glimpse --exact -r | ConvertFrom-Csv -Delimiter '|' -Header 'Name', 'Version'
+
+$latestVersion = [version]($Latest.tag_name -replace 'v', '')
+
+if($null -eq $Current) {
+	$Current = [pscustomobject]@{Version = '0.0.0'}
+}
+if([version]($Current.Version) -lt $latestVersion) {
+	$toolsDir = Join-Path $PSScriptRoot 'glimpse'
+	$nuspec = Get-ChildItem $toolsDir -Recurse -Filter '*.nuspec' | Select-Object -ExpandProperty FullName
+	$install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyinstall.ps1' | Select-Object -ExpandProperty FullName
+	$checksums = Invoke-RestMethod ($latest.assets | Where-Object name -eq 'sha256.txt').browser_download_url | ConvertFrom-Csv -Delimiter ' ' -Header 'checksum', 'filename'
+	$replacements = @(
+		@{
+			toReplace = '[[VERSION]]'
+			replaceWith = $latestVersion
+			file = $nuspec
+		},
+		@{
+			toReplace = '[[URL]]'
+			replaceWith = $($Latest.assets | Where-Object name -like '*i686*.msi' | Select-Object -ExpandProperty browser_download_url)
+			file = $install
+		},
+		@{
+			toReplace = '[[URL64]]'
+			replaceWith = $($Latest.assets | Where-Object name -like '*x64*.msi' | Select-Object -ExpandProperty browser_download_url)
+			file = $install
+		},
+		@{
+			toReplace = '[[CHECKSUM]]'
+			replaceWith = $($checksums | Where-Object filename -like '*i686*.msi' | Select-Object -ExpandProperty checksum)
+			file = $install
+		},
+		@{
+			toReplace = '[[CHECKSUM64]]'
+			replaceWith = $($checksums | Where-Object filename -like '*x64*.msi' | Select-Object -ExpandProperty checksum)
+			file = $install
+		}
+	)
+	foreach($currReplacement in $replacements){
+		(Get-Content $currReplacement.file).Replace($currReplacement.toReplace, $currReplacement.replaceWith) | Set-Content $currReplacement.file
+	}
+		
+#	(Get-Content "$install").Replace('[[URL]]', $($Latest.assets | Where-Object name -like '*i686*.msi' | Select-Object -ExpandProperty browser_download_url)) | Set-Content "$install"
+#	(Get-Content "$install").Replace('[[URL64]]', $($Latest.assets | Where-Object name -like '*x64*.msi' | Select-Object -ExpandProperty browser_download_url)) | Set-Content "$install"
+#	(Get-Content "$install").Replace('[[CHECKSUM]]', $($Latest.assets | Where-Object name -like '*x64*.msi' | Select-Object -ExpandProperty browser_download_url)) | Set-Content "$install"
+}
