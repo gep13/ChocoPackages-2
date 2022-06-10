40b3111f212d6025281ef81240e22514352ce22f
Cory Knox <me@coryknox.dev>
2022-06-10 14:27:12 -0700

    Don't include any files in wezterm

--- publishUpdates.ps1
+++ publishUpdates.ps1
@@ -2,7 +2,7 @@ param($ApiKey)
 choco find --source updatedPackages -r | ConvertFrom-Csv -Delimiter '|' -Header Name, Version | ForEach-Object {
     $existing = choco find $_.Name --source https://community.chocolatey.org/api/v2/ --version $_.Version --exact -r
     if ([string]::IsNullOrWhiteSpace($existing)) {
-        choco push "updatedPackages/$($_.Name)" --source https://push.chocolatey.org/ --key $ApiKey
+        choco push "updatedPackages/$($_.Name).$($_.Version).nupkg" --source https://push.chocolatey.org/ --key $ApiKey
     } else {
         Write-Host "$($_.Name) is already published to Community Repository, it's likely not yet approved."
     }
