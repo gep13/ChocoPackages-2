10552af642117a73fd147d4d311158ce22f790ca
Jeroen Wiert Pluimers <jeroen.github.com@pluimers.com>
2022-12-07 17:38:37 +0100

    Update from 1.22.0 to 1.24.1 part 6
    Migrate `chocolateyinstall.ps1` from deprecated `-file` and `-file64` to `-url` and `-url64bit` parameters and from embedded ZIP files (that were already gone) to downloads from the [v1.24.1 release URL](https://gitlab.com/gitlab-org/cli/-/releases/v1.24.1)

--- packages/glab.portable/tools/chocolateyinstall.ps1
+++ packages/glab.portable/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'glab_1.24.1_Windows_i386.zip'
-$fileLocation64 = Join-Path $toolsDir 'glab_1.24.1_Windows_x86_64.zip'
+$url = "https://gitlab.com/gitlab-org/cli/-/releases/v1.24.1/downloads/glab_1.24.1_Windows_i386.zip"
+$url64 = "https://gitlab.com/gitlab-org/cli/-/releases/v1.24.1/downloads/glab_1.24.1_Windows_x86_64.zip"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  file         = $fileLocation
-  file64         = $fileLocation64
+  url           = $url
+  url64bit      = $url64
   checksum      = '85e9b4ee96337ebe48b040a552b2cb7327be86d235ff6e2c9d555be32c9fe56c'
   checksumType  = 'sha256'
   checksum64    = '6b553a556ba7011a0c9fcf8a5f947ab60c6aabfaada47391ea7ba5703b08392b'
